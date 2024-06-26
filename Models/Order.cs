ae98570c9e1db969b5a6ed30aa40fb9b71d569fe
lobstore <63562913+lobstore@users.noreply.github.com>
2024-06-26 12:15:35 +0300

    Added validation

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,13 +1,21 @@
-﻿namespace TZVersta.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TZVersta.Models
 {
     public class Order
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Город отправителя обязателен для заполнения.")]
         public string? SenderCity { get; set; }
+        [Required(ErrorMessage = "Адрес отправителя обязателен для заполнения.")]
         public string? SenderAddress { get; set; }
+        [Required(ErrorMessage = "Город получателя обязателен для заполнения.")]
         public string? RecipientCity { get; set; }
+        [Required(ErrorMessage = "Адрес получателя обязателен для заполнения.")]
         public string? RecipientAddress { get; set; }
+        [Required(ErrorMessage = "Вес посылки обязателен для заполнения.")]
         public int Weight { get; set; }
+        [Required(ErrorMessage = "Дата получения обязательна для заполнения.")]
         public DateTime PickupDate { get; set; }
         public string? OrderNumber { get; set; }
     }
--- README.md
+++ README.md
@@ -7,7 +7,7 @@ dotnet add package Npgsql.EntityFrameworkCore.PostgreSQL
 dotnet add package Npgsql.EntityFrameworkCore.PostgreSQL.Design
 4. Установите pgadmin и postgresql сервер с официального сайта https://www.enterprisedb.com/downloads/postgres-postgresql-downloads 
 6. Пропишите указанные при установке postgresql данные в файле конфигурации appsettings.json, в строке подключения "DefaultConnection"
-7. Пропишите следующие команы в консоли для создания таблицы в БД и убедидесь в наличии соответствующей таблицы
+7. Пропишите следующие команды в консоли для создания таблицы в БД и убедидесь в наличии соответствующей таблицы
 dotnet ef migrations add InitialCreate
 dotnet ef database update
 8. Запустите проект с помощью dotnet run находясь в папке с файлом проекта
