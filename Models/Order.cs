51a9d28061a547c980a0cebfa1ddeaacb1a5b48d
lobstore <63562913+lobstore@users.noreply.github.com>
2024-06-26 17:22:33 +0300

    Fixed lables

--- Models/Order.cs
+++ Models/Order.cs
@@ -5,16 +5,28 @@ namespace TZVersta.Models
     public class Order
     {
         public int Id { get; set; }
+
+        [Display(Name = "Город отправителя")]
         [Required(ErrorMessage = "Город отправителя обязателен для заполнения.")]
         public string? SenderCity { get; set; }
+
+        [Display(Name = "Адрес отправителя")]
         [Required(ErrorMessage = "Адрес отправителя обязателен для заполнения.")]
         public string? SenderAddress { get; set; }
+
+        [Display(Name = "Город получателя")]
         [Required(ErrorMessage = "Город получателя обязателен для заполнения.")]
         public string? RecipientCity { get; set; }
+
+        [Display(Name = "Адрес получателя")]
         [Required(ErrorMessage = "Адрес получателя обязателен для заполнения.")]
         public string? RecipientAddress { get; set; }
-        [Required(ErrorMessage = "Вес посылки обязателен для заполнения.")]
+
+        [Display(Name = "Вес груза")]
+        [Required(ErrorMessage = "Вес груза обязателен для заполнения.")]
         public int Weight { get; set; }
+
+        [Display(Name = "Дата забора заказа")]
         [Required(ErrorMessage = "Дата получения обязательна для заполнения.")]
         public DateTime PickupDate { get; set; }
         public string? OrderNumber { get; set; }
